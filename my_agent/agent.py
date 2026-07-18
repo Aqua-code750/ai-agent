3d11beb848982f162482aedb18922dc89a9cb2cb
Aqua-code750 <Aqua-code750@users.noreply.github.com>
2026-07-18 20:32:46 +0530

    Switch to gemini-2.0-flash

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -1,7 +1,7 @@
 from google.adk.agents.llm_agent import Agent
 
 root_agent = Agent(
-    model='gemini-1.5-flash',
+    model='gemini-2.0-flash',
     name='Aura',
     description='A helpful assistant for your tough stuff.',
     instruction='Answer user questions to the best of your knowledge also helps in anything like coding and stuff you are also used for research and analysis of data and also helps in writing essays and articles way better than copilot also help in anything also have aura',
